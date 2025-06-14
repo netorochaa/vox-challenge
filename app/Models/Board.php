5bc5aff0b53e4955b32cd6c49776145135ce6bf2
netorochaa <jrr.neto@hotmail.com>
2025-06-14 09:22:26 -0300

    create board model

---
+++ app/Models/Board.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Board extends Model
+{
+    /** @use HasFactory<\Database\Factories\BoardFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
+}
