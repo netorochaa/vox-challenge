86798ff56c793b14b11f998754a6812f18d3d514
netorochaa <jrr.neto@hotmail.com>
2025-06-14 10:45:06 -0300

    install breeze and bootstrap

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', fn () => redirect()->route('login'));
+Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
+Route::post('login', [AuthenticatedSessionController::class, 'store']);
+
+Route::middleware('auth')->group(function () {
+    Route::view('home', 'home')->name('home');
+
+    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 });
