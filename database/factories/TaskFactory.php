6ca2a70e6dfcfe544325892414dabe69848cf74c
netorochaa <jrr.neto@hotmail.com>
2025-06-14 09:30:27 -0300

    create task model

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'name'        => $this->faker->unique()->sentence(),
+            'category_id' => Category::factory(),
+        ];
+    }
+}
